24adea8a2bfaa48ef6e0de98f339a4998b550fde
Jeepin95 <jeepin95@comcast.net>
2018-07-26 19:30:16 -0700

    Replaced library and code to the correct base version

--- src/PoolTimer.ino
+++ src/PoolTimer.ino
@@ -1,44 +1,74 @@
-
 // This #include statement was automatically added by the Particle IDE.
-#include <SparkIntervalTimer.h>
+#include <SparkCorePolledTimer.h>
 
-
-IntervalTimer runTimer;
 bool currentState = LOW;
-bool tempEnable = false;
-int nextTimer = 60;
+const long SEC = 1000;
+
+unsigned long lastSwitch = 0;
+unsigned long onDelay = 2*SEC*60;
+unsigned long offDelay = 10*SEC*60;
+unsigned long switchDelay = offDelay;
+
+int totalCycles = 0;
+bool heaterEnabled = false;
 
 void setup() {
-    Particle.publish("PoolTimerStartup");
-
+    Particle.publish("PoolHeaterStartup");
     Particle.subscribe("enablePoolTimer",enablePoolTimer);
     pinMode(6,OUTPUT);
-   // digitalWrite(6,HIGH);
-   runTimer.begin(runPool, 10000, hmSec);
+    digitalWrite(6,currentState);
+    Particle.variable("currentState",currentState);
+    Particle.variable("heaterEnabled",heaterEnabled);
 }
 
 void loop() {
-
-}
-
-void runPool() {
-    if(currentState == LOW) {
-        currentState = HIGH;
-        nextTimer = 60000;
-        Particle.publish("TurnOn2Minutes");
-    } else {
-        currentState = LOW;
-        nextTimer = 60000;
-        Particle.publish("TurnOff10Minutes");
+    if(heaterEnabled) {
+        switchPump();
     }
-    digitalWrite(6,currentState);
-    runTimer.resetPeriod_SIT(nextTimer,hmSec);
 }
+
+void switchPump() {
+    unsigned long now = millis();
+    if((now - lastSwitch) >= switchDelay) {
+        lastSwitch = now;
+        totalCycles++;
+        if(totalCycles > 2000) {
+            Particle.publish("MaxCyclesReached");
+            currentState = LOW;
+            heaterEnabled = false;
+            pumpOn(LOW);
+        } else {
+            if(currentState == LOW) {
+                currentState = HIGH;
+                switchDelay = onDelay;
+            } else {
+                currentState = LOW;
+                switchDelay = offDelay;
+            }
+            pumpOn(currentState);
+        }
+    }
+}
+
+void pumpOn(bool val) {
+    digitalWrite(6,val);
+    if(val) {
+        Particle.publish("PumpStatus","ON");
+    } else {
+        Particle.publish("PumpStatus","OFF");
+    }
+}
+
 
 void enablePoolTimer(const char *event, const char *data) {
-    if(currentState == LOW) {
+    if(data) {
+        totalCycles = 0;
+        heaterEnabled = true;
         currentState = HIGH;
+        pumpOn(currentState);
     } else {
+        heaterEnabled = false;
         currentState = LOW;
+        pumpOn(currentState);
     }
 }
